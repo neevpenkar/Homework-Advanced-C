8103e6d09dd3d29e742d19f7e8baea366965bf4e
neevpenkar <neev.penkar@gmail.com>
2022-03-25 00:02:04 +0200

    Source Number 2 has been created
    because of the change of the head pointer

--- Neev Visual/Homework Week 2/Homework Week 2/Source.cpp
+++ Neev Visual/Homework Week 2/Homework Week 2/Source.cpp
@@ -1,131 +1,131 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef union Date {
-	char hebdate[6];
-	unsigned int date;
-};
-typedef struct Worker {
-	unsigned int ID;
-	char* name;
-	int salary;
-	Date startdate;
-};
-typedef struct WorkerList {
-	Worker* workerdata;
-	struct WorkerList* next;
-};
-
-WorkerList* addWorker(WorkerList* head, Worker* worker) {
-	WorkerList* newworker = (WorkerList*)malloc(sizeof(WorkerList));
-	newworker->workerdata = worker;
-	newworker->next = head;
-	return newworker;
-}
-void printWorker(Worker* w, int flag)
-{
-	printf("ID: %d\n", w->ID);
-	printf("Name: %s\n", w->name);
-	printf("Salary: %d\n", w->salary);
-	if (flag == 0)
-		printf("%s\n", w->startdate.hebdate);
-	else
-		printf("Start Date: %d\n", w->startdate.date);
-}
-void printList(WorkerList *list) {
-	
-	while (list != NULL) {
-		printWorker(list->workerdata, 1);
-		list = list->next;
-	}
-	printf("---------------------\n");
-	printWorker(list->workerdata, 1);
-}
-Worker* createWorker(int id, char* name1, int salary, Date startdate) {
-	Worker* newworker;
-	newworker = (Worker*)malloc(sizeof(Worker));
-
-	newworker->ID = id;
-	newworker->salary = salary;
-
-	newworker->name = (char*)malloc(strlen(name1) + 1);
-	if (!newworker->name) exit(-1);
-	strcpy(newworker->name, name1);
-	//newworker.name[strlen(name1)] = '\0';
-
-	newworker->startdate = startdate;
-	return newworker;
-}
-
-//WorkerList* deleteWorstWorker(WorkerList* head) {
-//	/*Worker* worstworker = head->workerdata, *secondworstworker = NULL;
-//	secondworstworker = head->workerdata;
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 //
-//	while (head != NULL) {
-//		if ()
-//	}*/
+//typedef union Date {
+//	char hebdate[6];
+//	unsigned int date;
+//};
+//typedef struct Worker {
+//	unsigned int ID;
+//	char* name;
+//	int salary;
+//	Date startdate;
+//};
+//typedef struct WorkerList {
+//	Worker* workerdata;
+//	struct WorkerList* next;
+//};
 //
+//WorkerList* addWorker(WorkerList* head, Worker* worker) {
+//	WorkerList* newworker = (WorkerList*)malloc(sizeof(WorkerList));
+//	newworker->workerdata = worker;
+//	newworker->next = head;
+//	return newworker;
 //}
-int index (WorkerList* head, long unsigned id) {
-	int count = 0;
-	for (int i = 0; head != NULL; i++) {
-		if (head->workerdata->ID == id)
-			return i;
-		head = head->next;
-	}
-	return -1;
-	/*while (head->workerdata->ID != id) {
-		count++;
-		head = head->next;
-		if (head == NULL)
-			return -1;
-	}*/
-	//return count;
-}
-int recursiveIndex(WorkerList* head, long unsigned id) {
-	if (head == NULL) return -1;
-
-	if (head->workerdata->ID == id) return 1;
-	
-	if (recursiveIndex(head->next, id) == -1)
-		return -1;
-	else return recursiveIndex(head->next, id) + 1;
-}
-void updateWorkers(WorkerList* head, int percent) {
-	while (head != NULL) {
-		//head->workerdata->salary *= (percent / 100);
-		printf("%d", head->workerdata->salary);
-		head = head->next;
-	}
-}
-int main() {
-
-	Worker *newworker, *worker2, *worker3, *worker4;
-	
-	Date date1 = { 2022 };
-	char name[] = "Adan Esaid";
-	char name2[] = "Neev Penkar";
-	char name3[] = "Lolo Esaid";
-	char name4[] = "Bibi Netanyahu";
-	int flag = 1;
-
-	newworker = createWorker(3412, name, 20000, date1);
-	worker2 = createWorker(4384, name2, 19999, date1);
-	worker3 = createWorker(8967, name3, 21000, date1);
-	worker4 = createWorker(2389, name4, 00012, date1); // BIBI
-
-	WorkerList* list = (WorkerList*)malloc(sizeof(WorkerList));
-	list = addWorker(NULL, newworker);
-	list = addWorker(list, worker2);
-	list = addWorker(list, worker3);
-	list = addWorker(list, worker4);
-
-	//deleteWorstWorker(list);
-	printList(list);
-	updateWorkers(list, 110); // Increment will be all values over 100 percent. Decrement for all values below.
-	printList(list);
-	
-	return 0;
-}
+//void printWorker(Worker* w, int flag)
+//{
+//	printf("ID: %d\n", w->ID);
+//	printf("Name: %s\n", w->name);
+//	printf("Salary: %d\n", w->salary);
+//	if (flag == 0)
+//		printf("%s\n", w->startdate.hebdate);
+//	else
+//		printf("Start Date: %d\n", w->startdate.date);
+//}
+//void printList(WorkerList *list) {
+//	
+//	while (list != NULL) {
+//		printWorker(list->workerdata, 1);
+//		list = list->next;
+//	}
+//	printf("---------------------\n");
+//	printWorker(list->workerdata, 1);
+//}
+//Worker* createWorker(int id, char* name1, int salary, Date startdate) {
+//	Worker* newworker;
+//	newworker = (Worker*)malloc(sizeof(Worker));
+//
+//	newworker->ID = id;
+//	newworker->salary = salary;
+//
+//	newworker->name = (char*)malloc(strlen(name1) + 1);
+//	if (!newworker->name) exit(-1);
+//	strcpy(newworker->name, name1);
+//	//newworker.name[strlen(name1)] = '\0';
+//
+//	newworker->startdate = startdate;
+//	return newworker;
+//}
+//
+////WorkerList* deleteWorstWorker(WorkerList* head) {
+////	/*Worker* worstworker = head->workerdata, *secondworstworker = NULL;
+////	secondworstworker = head->workerdata;
+////
+////	while (head != NULL) {
+////		if ()
+////	}*/
+////
+////}
+//int index (WorkerList* head, long unsigned id) {
+//	int count = 0;
+//	for (int i = 0; head != NULL; i++) {
+//		if (head->workerdata->ID == id)
+//			return i;
+//		head = head->next;
+//	}
+//	return -1;
+//	/*while (head->workerdata->ID != id) {
+//		count++;
+//		head = head->next;
+//		if (head == NULL)
+//			return -1;
+//	}*/
+//	//return count;
+//}
+//int recursiveIndex(WorkerList* head, long unsigned id) {
+//	if (head == NULL) return -1;
+//
+//	if (head->workerdata->ID == id) return 1;
+//	
+//	if (recursiveIndex(head->next, id) == -1)
+//		return -1;
+//	else return recursiveIndex(head->next, id) + 1;
+//}
+//void updateWorkers(WorkerList* head, int percent) {
+//	while (head != NULL) {
+//		//head->workerdata->salary *= (percent / 100);
+//		printf("%d", head->workerdata->salary);
+//		head = head->next;
+//	}
+//}
+//int main() {
+//
+//	Worker *newworker, *worker2, *worker3, *worker4;
+//	
+//	Date date1 = { 2022 };
+//	char name[] = "Adan Esaid";
+//	char name2[] = "Neev Penkar";
+//	char name3[] = "Lolo Esaid";
+//	char name4[] = "Bibi Netanyahu";
+//	int flag = 1;
+//
+//	newworker = createWorker(3412, name, 20000, date1);
+//	worker2 = createWorker(4384, name2, 19999, date1);
+//	worker3 = createWorker(8967, name3, 21000, date1);
+//	worker4 = createWorker(2389, name4, 00012, date1); // BIBI
+//
+//	WorkerList* list = (WorkerList*)malloc(sizeof(WorkerList));
+//	list = addWorker(NULL, newworker);
+//	list = addWorker(list, worker2);
+//	list = addWorker(list, worker3);
+//	list = addWorker(list, worker4);
+//
+//	//deleteWorstWorker(list);
+//	printList(list);
+//	updateWorkers(list, 110); // Increment will be all values over 100 percent. Decrement for all values below.
+//	printList(list);
+//	
+//	return 0;
+//}
---
+++ Neev Visual/Homework Week 2/Homework Week 2/Source2.cpp
@@ -0,0 +1,81 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef union Date {
+	int date;
+	char hebYear[4];
+};
+typedef struct Worker {
+	int id;
+	int salary;
+	char* name;
+	Date startDate;
+};
+typedef struct WorkerList{
+	Worker* workerdata;
+	WorkerList* next;
+};
+Worker* createWorker(int id, int salary, char* name, Date date) {
+	Worker* tempWorker = (Worker*)malloc(sizeof(Worker));
+	
+	// Filling the fields of the new object with info given by the params.
+
+	tempWorker->name = (char*)malloc(strlen(name) + 1);
+	strcpy(tempWorker->name, name);
+	*(tempWorker->name + strlen(name)) = '\0';
+
+	tempWorker->id = id;
+	tempWorker->salary = salary;
+
+	return tempWorker;
+}
+void printWorker(Worker* worker, int flag) {
+	// printf("------------------------\n");
+	printf("ID: %d\n", worker->id);
+	printf("Name: %s\n", worker->name);
+	printf("Salary: %d\n", worker->salary);
+	printf("------------------------\n");
+	return;
+}
+WorkerList* addWorker2List(WorkerList* head, Worker* newworker) {
+	// אני לא יודע איך לבדוק אם ברשימה יש איברים לכן הנחתי שיש איבר כלשהו שמצביע לNULL
+	WorkerList* temphead = head;
+	WorkerList* templist = (WorkerList*)malloc(sizeof(WorkerList));
+	templist->workerdata = newworker;
+	templist->next = temphead;
+	return templist;
+}
+int Workerindex(WorkerList* head, short int id) {
+	WorkerList* templist = head;
+	if (templist == NULL) return -1;
+
+	int count = 1;
+	while (templist->workerdata->id != id) {
+		count++;
+		templist = templist->next;
+		if (templist == NULL) return -1;
+	}
+	return count;
+}
+int main() {
+	char name1[] = "Neev Penkar";
+	char name2[] = "Adan Esaid";
+	char name3[] = "Bibi Netanyahu";
+
+	Worker* newworker1 = createWorker(841, 200, name1, {2020});
+	Worker* newworker2 = createWorker(763, 4000, name2, { 2019 });
+	Worker* newworker3 = createWorker(925, 40000, name3, { 1996 });
+	
+
+	WorkerList* list = (WorkerList*)malloc(sizeof(WorkerList));
+	*list = {newworker1, NULL};
+	list = addWorker2List(list, newworker2);
+	list = addWorker2List(list, newworker3);
+
+	// printWorker(list->workerdata, 1);
+	printf("Index: %d", Workerindex(list, 763));
+	
+	return 0;
+}
