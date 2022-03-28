1320293772fda97d45bd3474e834cc7cd9444d87
neevpenkar <neev.penkar@gmail.com>
2022-03-28 15:28:06 +0300

    SOF SOF Add worker sorted

--- Neev Visual/Homework Week 2/Homework Week 2/Source2.cpp
+++ Neev Visual/Homework Week 2/Homework Week 2/Source2.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 typedef union Date {
 	int date;
 	char hebYear[4];
@@ -59,23 +58,80 @@ int Workerindex(WorkerList* head, short int id) {
 	}
 	return count;
 }
+void printList(WorkerList* head) {
+	WorkerList* temphead = head;
+	while (temphead) {
+		printWorker(temphead->workerdata, 1);
+		temphead = temphead->next;
+	}
+	return;
+}
+WorkerList* deleteWorstWorker(WorkerList* head) {
+	//Worker* worst = head->workerdata;
+	WorkerList* tmp = head, * temp;
+	temp = head;
+	if (!head)
+		return head;
+	while (tmp->next != NULL) {
+		if (tmp->next->workerdata->salary < temp->workerdata->salary)
+			temp = tmp;
+		tmp = tmp->next;
+	}
+	if (tmp->workerdata->salary < temp->workerdata->salary) {
+		temp = tmp;
+		temp->next = temp->next->next;
+	}
+	else
+		temp->next = temp->next->next;
+	free(temp);
+	return head;
+}
+
+WorkerList* addWorker(WorkerList* head, Worker* newworker) {
+	WorkerList* newlist = (WorkerList*)malloc(sizeof(WorkerList));
+	newlist->workerdata = newworker;
+	WorkerList* temp = head;
+
+	while (temp->workerdata->salary > newworker->salary) {
+		if (temp->next == NULL) break;
+		temp = temp->next;
+	}
+	if (temp == head) {
+		newlist->next = head;
+		return newlist;
+	}
+	else {
+		newlist->next = temp->next;
+		temp->next = newlist;
+		return head;
+	}
+}
+
 int main() {
 	char name1[] = "Neev Penkar";
 	char name2[] = "Adan Esaid";
 	char name3[] = "Bibi Netanyahu";
+	char name4[] = "Lukkha";
 
 	Worker* newworker1 = createWorker(841, 200, name1, {2020});
 	Worker* newworker2 = createWorker(763, 4000, name2, { 2019 });
 	Worker* newworker3 = createWorker(925, 40000, name3, { 1996 });
-	
+	Worker* worker4 = createWorker(487, 1200, name4, { 1996 });
 
 	WorkerList* list = (WorkerList*)malloc(sizeof(WorkerList));
 	*list = {newworker1, NULL};
-	list = addWorker2List(list, newworker2);
-	list = addWorker2List(list, newworker3);
 
-	// printWorker(list->workerdata, 1);
-	printf("Index: %d", Workerindex(list, 763));
-	
+	list = addWorker(list, worker4);
+	list = addWorker(list, newworker2);
+	list = addWorker(list, newworker3);
+	printList(list);
 	return 0;
+}
+void foo() {
+	//WorkerList* list;
+	////printList(list);
+	//list = addWorker(list, newworker2);
+	////printList(list);
+	//list = addWorker(list, newworker3);
+	////printList(list);
 }
